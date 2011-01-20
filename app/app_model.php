f32f3c780b01aa86f4ef74fcf21a47dcbde13efd
huoxito <huoxito@gmail.com>
2011-01-20 11:10:03 -0300

    adiciono função updateContas

--- app/app_model.php
+++ app/app_model.php
@@ -2,8 +2,18 @@
     
     
 class AppModel extends Model{
-
     
+    function updateContas($object, $data, $sinal){
+        
+        $conditions = array('Conta.usuario_id' => $data['usuario_id'],
+                            'Conta.id' => $data['conta_id']);
+        $values = array('saldo' => 'saldo' . $sinal . $data['valor']);
+        if( $object->Conta->updateAll($values, $conditions) ){
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
 
 
