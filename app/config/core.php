5701af74a99024016af508e5bc50f68b801a82e9
huoxito <huoxito@gmail.com>
2011-08-06 20:06:49 -0300

    um dia pra session timeout

--- app/config/core.php
+++ app/config/core.php
@@ -178,7 +178,7 @@
  * Session time out time (in seconds).
  * Actual value depends on 'Security.level' setting.
  */
-	Configure::write('Session.timeout', '9');
+	Configure::write('Session.timeout', '864');
 
 /**
  * If set to false, sessions are not automatically started.
